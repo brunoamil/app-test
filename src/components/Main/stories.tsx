07f00f244f08f3584fec1387e8dec75a42abe9a8
Bruno Lima <bruno-amil@hotmail.com>
2020-10-27 02:08:57 -0300

    configuracao do storybook

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,16 +1,18 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { withKnobs, text } from '@storybook/addon-knobs'
 import Main from '.'
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: 'title default',
+    description: 'description default'
+  }
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'Bruninho')}
-    description={text('Description', 'Safasdsdo')}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'title basic',
+  description: 'description basic'
+}
+export const Default: Story = (args) => <Main {...args} />
