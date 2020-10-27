21f70fd1604ec0e7be3a27ebeab49722f9721a9e
Bruno Lima <bruno-amil@hotmail.com>
2020-10-27 01:16:09 -0300

    configurando o storybook

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -12,4 +12,10 @@ describe('<Main />', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('Testando o background', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
 })
